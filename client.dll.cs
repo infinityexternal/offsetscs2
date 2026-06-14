e88aa351ff6c7267aaac82e523f25fcf277aaa0c
infinityexternal <contact.infinityexternal@gmail.com>
2026-06-14 15:15:04 -0300

    Add files via upload

---
+++ client.dll.cs
@@ -0,0 +1,42 @@
+// client.dll.cs - netvars de classe (m_...)
+
+// jogador / pawn
+m_hPlayerPawn    = 0x90C
+m_iHealth        = 0x34C
+m_vOldOrigin     = 0x1390
+m_iTeamNum       = 0x3EB
+m_lifeState      = 0x354
+m_modelState     = 0x150
+m_pGameSceneNode = 0x330
+m_iszPlayerName  = 0x6F4
+m_vecVelocity    = 0x430
+
+// spectator list
+m_hObserverPawn     = 0x910
+m_pObserverServices = 0x11F8
+m_hObserverTarget   = 0x4C
+
+// anti-flash
+m_flFlashBangTime = 0x13EC
+
+// bomba (C_PlantedC4)
+m_bBombTicking      = 0x1160
+m_bBombDefused      = 0x11B4
+m_nBombSite         = 0x1164
+m_flC4Blow          = 0x1190
+m_hBombDefuser      = 0x11B8
+m_flDefuseCountDown = 0x11B0
+
+// origin de entidade nao-pawn
+m_nodeAbsOrigin = 0xC8
+
+// granadas
+m_pEntity      = 0x10
+m_designerName = 0x20
+
+// radar / spotted
+m_entitySpottedState = 0x1C38
+m_bSpotted           = 0x8
+
+// posicao do olho (reserva)
+m_vecViewOffset = 0xE70
---
+++ manual.cs
@@ -0,0 +1,27 @@
+// manual.cs - offsets que nao saem de dump (curtime + bones)
+
+// curtime: offset interno dentro de CGlobalVarsBase (hex)
+curtime = 0x30
+
+// bones: indices do boneMatrix (decimal). -1 = ignora (aproxima)
+boneChest = -1
+
+boneHead   = 7
+boneNeck   = 6
+boneSpine  = 5
+bonePelvis = 1
+
+boneShoulderL = 9
+boneElbowL    = 10
+boneHandL     = 11
+
+boneShoulderR = 13
+boneElbowR    = 14
+boneHandR     = 15
+
+boneHipR  = 17
+boneKneeR = 18
+boneFootR = 19
+boneHipL  = 20
+boneKneeL = 21
+boneFootL = 22
