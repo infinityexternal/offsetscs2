d89ce3bd651ed1947eb0fafd895b56dc51ca764a
infinityexternal <contact.infinityexternal@gmail.com>
2026-07-03 13:27:13 -0300

    Update offets.cs

--- offets.cs
+++ offets.cs
@@ -1,8 +1,8 @@
 // offsets.cs - globais por assinatura (dw...)
-dwLocalPlayerPawn = 0x2341698
-dwEntityList      = 0x24E76A0
-dwViewMatrix      = 0x2346B30
-dwPlantedC4       = 0x234FF98
+dwLocalPlayerPawn = 0x2341528
+dwEntityList      = 0x24E7680
+dwViewMatrix      = 0x23469C0
+dwPlantedC4       = 0x234FE28
 dwGlobalVars      = 0x20616D0
-dwViewAngles      = 0x23568C8
-dwForceAttack     = 0x2064A90
+dwViewAngles      = 0x2356748
+dwForceAttack     = 0x2065A90
